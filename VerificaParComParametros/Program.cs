eea9639cb763f206854c41c727d050daad21e5c7
Daniel Visicatto <dpfreestyle@gmail.com>
2023-03-06 11:38:14 -0300

    Add project files.

---
+++ VerificaParComParametros/Program.cs
@@ -0,0 +1,30 @@
+﻿int number;
+interacaoUsuario();
+respostaUsuario();  
+
+// função com apenas um retorno:
+bool ehPAr(int valor)
+{
+    bool answer = false;
+    if( valor % 2 ==0)
+    {
+        answer = true;
+    }
+    return answer;
+}
+void interacaoUsuario()
+{
+    Console.Write("Informe um número para verificar se é par ou ímpar: ");
+    if (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("Valor inválido!!\n" +
+                          "Preciso de um valor inteiro!");
+    }else
+        ehPAr(number);
+}
+void respostaUsuario()
+{
+    if (ehPAr(number))
+        Console.WriteLine("{0}, é par!", number);
+    else Console.WriteLine("{0} é impar!", number);    
+}
